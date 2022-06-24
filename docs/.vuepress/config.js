c8f178d10a0c860d12dd98a2aaf7bb770ddfd68f
xiashoucong <xiashoucong@searchxyz.com.cn>
2022-06-24 17:24:12 +0800

    init conmmit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    lang: 'zh-CN',
+    title: '你好， VuePress ！',
+    description: '这是我的第一个 VuePress 站点',
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+          { text: 'External', link: 'https://google.com' },
+        ]
+      }
+  }
