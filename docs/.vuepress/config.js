8c4b0f66d7b3a79b2c04078f05e5a3bedd625110
violinxsc <1801698054@qq.com>
2022-06-24 23:32:19 +0800

    base config test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,10 +3,40 @@ module.exports = {
     title: '你好， VuePress ！',
     description: '这是我的第一个 VuePress 站点',
     themeConfig: {
+        logo: '/assets/img/logo.png',
         nav: [
           { text: 'Home', link: '/' },
           { text: 'Guide', link: '/guide/' },
-          { text: 'External', link: 'https://google.com' },
-        ]
+          { text: 'External', link: 'https://baidu.com' },
+          {
+            text: 'Languages',
+            ariaLabel: 'Language Menu',
+            items: [
+              { text: '中文', link: '/language/zh/' },
+              { text: 'ENG', link: '/language/en/' }
+            ]
+          }
+        ],
+        sidebar: {
+          '/foo/': [
+            '',     /* /foo/ */
+            'one',  /* /foo/one.html */
+            'two'   /* /foo/two.html */
+          ],
+    
+          '/bar/': [
+            '',      /* /bar/ */
+            'three', /* /bar/three.html */
+            'four'   /* /bar/four.html */
+          ],
+    
+          // fallback
+          '/': [
+            '',        /* / */
+            'contact', /* /contact.html */
+            'about'    /* /about.html */
+          ]
+        },
+        lastUpdated: 'Last Updated',
       }
   }
